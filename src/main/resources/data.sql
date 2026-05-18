0df2e38c6253b86888a47892a92102f5f4a33d7d
Maicon A. Jagucheski <maj.ski@hotmail.com>
2026-05-18 15:04:40 -0300

    #Add readme

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO usuarios (id, nome, email, password, secret, autenticacao_2f)
-SELECT 1, 'Maicon Aessio Jagucheski', 'maiconjagucheski@gmail.com', '123456', null, false
-    WHERE NOT EXISTS (SELECT 1 FROM usuarios WHERE email = 'maiconjagucheski@gmail.com');
+SELECT 1, 'Usuario teste', 'usuario@gmail.com', '123456', null, false
+    WHERE NOT EXISTS (SELECT 1 FROM usuarios WHERE email = 'usuario@gmail.com');
 
 -- INSERT INTO usuarios (id, nome, email, password, secret, autenticacao_2f)
--- VALUES (1, 'Maicon Aessio Jagucheski', 'maiconjagucheski@gmail.com', '123456', null, false)
+-- VALUES (1, 'Usuario teste', 'usuario@gmail.com'', '123456', null, false)
